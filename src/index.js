355350e9e903a13761e9869c945f8deb9b72dfcf
4Green <yaqqww@nate.com>
2020-02-11 22:25:37 +0900

    innerHTML이 아닌 appendChild를 이용하여 요소 추가

--- src/index.js
+++ src/index.js
@@ -1,41 +1,65 @@
 import Memo from './js/Memo';
 import './css/Memo.css';
 
-const wrap = document.querySelector('#Wrap');
+const setting = {
+  init: () => {
+    const wrap = document.querySelector('#Wrap');
 
-if (localStorage.length === 0 || localStorage.getItem('listItems')[0] !== '[') {
-  const memo = new Memo('a라는 메모', 200, 200, 200, 200, 0);
-  wrap.innerHTML = memo.getMemoNodeElement();
-} else {
-  let el = '';
-  const listItems = JSON.parse(localStorage.getItem('listItems'));
-  listItems.forEach(iter => {
-    const { content, width, height, top, left, zIndex } = iter;
-    const memo = new Memo(content, width, height, top, left, zIndex);
-    el = el + memo.getMemoNodeElement();
-  });
-  wrap.innerHTML = el;
+    if (localStorage.length === 0 || localStorage.getItem('listItems')[0] !== '[') {
+      const memo = new Memo('a라는 메모', 200, 200, 200, 200, 0);
+      const clone = memo.getMemoNodeElement();
+      wrap.appendChild(clone);
+    } else {
+      const listItems = JSON.parse(localStorage.getItem('listItems'));
+
+      listItems.forEach(iter => {
+        const { content, width, height, top, left, zIndex } = iter;
+        const memo = new Memo(content, width, height, top, left, zIndex);
+        const clone = memo.getMemoNodeElement();
+        wrap.appendChild(clone);
+      });
+
+    }
+  }
 }
+
+setting.init();
 
 // modck data
 
 // const localStorageItem = [
-//     {
-//       content: 'a라는 메모',
-//       width: 200,
-//       height: 200,
-//       top: 200,
-//       left: 200,
-//       zIndex: 0
-//     },
-//     {
-//       content: 'b라는 메모',
-//       width: 200,
-//       height: 200,
-//       top: 500,
-//       left: 500,
-//       zIndex: 1
-//     }
-//   ]
+//   {
+//     content: 'a라는 메모',
+//     width: 200,
+//     height: 200,
+//     top: 200,
+//     left: 200,
+//     zIndex: 0
+//   },
+//   {
+//     content: 'b라는 메모',
+//     width: 200,
+//     height: 200,
+//     top: 500,
+//     left: 500,
+//     zIndex: 1
+//   },
+//   {
+//     content: 'c라는 메모',
+//     width: 200,
+//     height: 200,
+//     top: 80,
+//     left: 500,
+//     zIndex: 2
+//   },
+//   {
+//     content: 'd라는 메모',
+//     width: 200,
+//     height: 200,
+//     top: 80,
+//     left: 860,
+//     zIndex: 3
+//   }
+// ]
 
 // localStorage.setItem('listItems', JSON.stringify(localStorageItem));
