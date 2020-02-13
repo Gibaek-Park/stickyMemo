46c9e83f1d42b680fa0027cfbaba0bc056456fdf
4Green <yaqqww@nate.com>
2020-02-13 23:59:23 +0900

    init 변경

--- src/js/Memo.js
+++ src/js/Memo.js
@@ -18,7 +18,24 @@ class Memo {
     });
   }
 
-  getMemoNodeElement() {
+  static init() {
+
+    if (localStorage.length === 0 || localStorage.getItem('listItems')[0] !== '[') {
+      const memo = new Memo('a라는 메모', 200, 200, 200, 200, 0);
+      memo.setMemo({ ...memo });
+    } else {
+      const listItems = JSON.parse(localStorage.getItem('listItems'));
+      listItems.forEach(iter => {
+        const { content, width, height, top, left, zIndex } = iter;
+        const memo = new Memo(content, width, height, top, left, zIndex);
+        memo.setMemo({ ...iter });
+      });
+    }
+  }
+
+  setMemo({ content, width, height, top, left, zIndex }) {
+    const wrap = document.querySelector('#Wrap');
+
     const temp = document.getElementsByTagName("template")[0];
     const clone = temp.content.cloneNode(true);
     const cloneWrap = clone.querySelector('.memo');
@@ -28,7 +45,7 @@ class Memo {
     cloneTextArea.setAttribute('style', `width:${this.width}px;height:${this.height}px`);
     cloneTextArea.innerText = this.content;
 
-    return clone;
+    wrap.appendChild(clone);
   }
   getMemoListItems() {
     // 현재 momo listItems를 return
