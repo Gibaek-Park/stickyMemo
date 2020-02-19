9df7112b7bfce91cac7e519adc6b2c176469df7e
4Green <yaqqww@nate.com>
2020-02-20 02:12:58 +0900

    targetNode 에 관련 util 추가

---
+++ utils/targetNode.js
@@ -0,0 +1,12 @@
+export const targetNode = (e) => {
+  const wrap = document.querySelector('#wrap');
+
+  Array.from(wrap.children).forEach((iter, idx) => {
+    iter.setAttribute('index', idx);
+  });
+
+  const index = Number(e.currentTarget.parentNode.parentNode.getAttribute('index'));
+  const target = wrap.children[index];
+
+  return [index, target];
+}
