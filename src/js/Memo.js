1f7ac70b7ec66ab2a40b3d430a0ce22c7da89310
4Green <yaqqww@nate.com>
2020-02-22 23:33:41 +0900

    dom 조작 시 memolistItems도 함께 변경

--- src/js/Memo.js
+++ src/js/Memo.js
@@ -15,15 +15,15 @@ class Memo {
       memo.createMemo();
     } else {
       const listItems = JSON.parse(localStorage.getItem('listItems'));
-      listItems.forEach(iter => {
+      listItems.forEach((iter, idx) => {
         const { content, width, height, top, left } = iter;
         const memo = new Memo(content, width, height, top, left);
-        memo.createMemo();
+        memo.createMemo(idx);
       });
     }
   }
 
-  createMemo() {
+  createMemo(idx) {
     const wrap = document.querySelector('#wrap');
     const temp = document.getElementsByTagName("template")[0];
     const clone = temp.content.cloneNode(true);
@@ -32,10 +32,12 @@ class Memo {
     const cloneTextArea = cloneWrap.querySelector('.textarea');
 
     cloneWrap.setAttribute('style', `top:${this.top}px;left:${this.left}px;`);
+    cloneWrap.setAttribute('index', idx);
     cloneTextArea.setAttribute('style', `width:${this.width}px;height:${this.height}px`);
     cloneTextArea.textContent = this.content;
 
     cloneWrap.addEventListener('contextmenu', e => this.add(e, cloneWrap));
+    cloneWrap.addEventListener('click', e => this.setPosition(e));
     btnClose.addEventListener('click', e => this.remove(e));
     cloneTextArea.addEventListener('keyup', e => this.editTextArea(e));
 
@@ -57,11 +59,14 @@ class Memo {
     const btnClose = clone.querySelector('.btn_close');
     const textArea = target.children[1].children[0];
     const { width, height } = textArea.getBoundingClientRect();
+    const MaxIndex = wrap.children.length;
 
-    clone.style.cssText = `top: ${top}px;left: ${left}px`;
+    clone.setAttribute('style', `top:${top}px;left:${left}px`);
+    clone.setAttribute('index', MaxIndex);
     cloneTextArea.textContent = '';
 
     clone.addEventListener('contextmenu', e => this.add(e, clone));
+    clone.addEventListener('click', e => this.setPosition(e));
     btnClose.addEventListener('click', e => this.remove(e));
     cloneTextArea.addEventListener('keyup', e => this.editTextArea(e));
 
@@ -75,21 +80,38 @@ class Memo {
 
     wrap.appendChild(clone);
   };
+  setPosition(e) {
+    const wrap = document.querySelector('#wrap');
+    const target = e.currentTarget;
+    const index = Number(target.getAttribute('index'));
+
+    if (typeof wrap.children[index] !== 'undefined' && wrap.children[index] === target) {
+      const textArea = target.querySelector('.textarea');
+      const content = textArea.textContent;
+
+      wrap.appendChild(target);
+      textArea.focus();
+      textArea.textContent = content;
+
+      this.sortMemoListItems(index);
+    }
+  };
+
   remove(e) {
-    const [index, target] = targetNode(e);
+    const target = e.target.parentNode.parentNode;
+    const index = Number(target.getAttribute('index'));
 
     wrap.removeChild(target);
     this.updateMemoListItems(index);
+    this.sortMemoListItems(index, 'DELETE');
   };
-
   editTextArea(e) {
     const content = e.target.textContent;
-    const [index, target] = targetNode(e);
+    const index = Number(e.target.parentNode.parentNode.getAttribute('index'));
     const listItems = Memo.listItems.map((item, idx) => idx === index ? { ...item, content } : item);
 
     this.setMemoListItems(listItems);
-  }
-
+  };
   getMemoListItems() {
     return Memo.listItems;
   };
@@ -105,6 +127,32 @@ class Memo {
     Memo.listItems = listItems;
     localStorage.setItem('listItems', JSON.stringify(listItems));
   };
+  sortMemoListItems(index, type) {
+    let newArr = [];
+    let tempVal = {};
+    const wrap = document.querySelector('#wrap');
+    // index 재 정의 겸 listItems 재 정렬
+
+    if (type !== 'DELETE') {
+      Array.from(wrap.children).forEach((iter, idx) => {
+        iter.setAttribute('index', idx);
+      });
+
+      Memo.listItems.forEach((item, idx) => {
+        if (idx === index) {
+          tempVal = item;
+        } else {
+          newArr.push(item);
+        }
+      });
+
+      Object.keys(tempVal).length > 0 && newArr.push(tempVal);
+
+      this.setMemoListItems(newArr);
+    }
+
+  };
+
 };
 
 export default Memo;
